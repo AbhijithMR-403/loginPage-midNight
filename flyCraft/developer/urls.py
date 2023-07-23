a6d100103a1a2612a21b76ff3cc6aeddacb69cb2
Abhijith MR <abhijithmr581@gmail.com>
2023-07-23 20:03:04 +0530

    The first commit

---
+++ flyCraft/developer/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from developer import views
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    # path('about',views.about,name='about'),
+    # path('contact',views.contact,name='contact'),
+    # path('blog',views.blog,name='blog'),
+    # path('services',views.services,name='services'),
+    # path('login',views.handlelogin,name='handlelogin'),
+    # path('logout',views.handlelogout,name='handlelogout'),
+    # path('signup',views.handlesignup,name='handlesignup'),
+    
+
+    
+
+]
